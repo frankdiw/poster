df2eeebd22eb5012b16908a8e03e2b7bc06ee145
frankdiw <57272276@qq.com>
2024-02-27 19:34:36 +0800

    优化解析

--- index.ts
+++ index.ts
@@ -1,20 +1,11 @@
 import { createCanvas } from "canvas";
 import fs from "fs";
 import { drawParagraph } from "./utils/paragraph";
+import { parse } from "./libs/parse";
 const canvas = createCanvas(750, 1392);
 const ctx = canvas.getContext("2d");
 
 (async () => {
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, 750, 1392);
-  drawParagraph(ctx,
-    {
-      x: 0, y: 0, text: '轻量应用服务器轻量应用服务器轻量应用服务器轻量应用服务器ceisgdastduasdas轻量应用服务器轻量应用服务器轻量应用服务器轻量应用服务器ceisgdastduasdas', width: 400,
-      font: { weight: 'bold', size: 32, color: 'red' }, lineClamp: 3, ellipsis: true,
-      opacity: 0.5,
-      lineHeight: 48
-    })
-  const out = fs.createWriteStream(__dirname + "/test.jpeg");
-  const stream = canvas.createJPEGStream();
-  stream.pipe(out);
+  const result= parse(fs.readFileSync('./index.jsx',{encoding:'utf-8'}), {a:1})
+  console.log(JSON.stringify(result,null,2))
 })();
